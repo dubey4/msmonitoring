784c70f63a7534d00402cbe6a50842d2330f77a8
vivek <vivek.dubeydeveloper@gmail.com>
2021-11-01 15:04:42 +0530

    commit project

---
+++ productms/src/main/java/com/iitj/productms/ProductmsApplication.java
@@ -0,0 +1,13 @@
+package com.iitj.productms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmsApplication.class, args);
+	}
+
+}
---
+++ productms/src/test/java/com/iitj/productms/ProductmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitj.productms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
