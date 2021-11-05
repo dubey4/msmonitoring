311a4940cdff3bd4b3bd735bdb87e1301f681aca
vivek <vivek.dubeydeveloper@gmail.com>
2021-11-05 13:58:52 +0530

    commit for UI

---
+++ eshopping/src/test/java/com/iitj/eshopping/EshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitj.eshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
