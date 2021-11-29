888b45ad6eeb0193d85a8001bce3724c0746a568
vivek <vivek.dubeydeveloper@gmail.com>
2021-11-30 00:17:56 +0530

    add discovery

---
+++ discovery/src/test/java/com/iitj/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitj.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
